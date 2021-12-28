8183c89e0c4702d0824e02198693b424f5221e52
“prakshh” <prakashdas18@gmail.com>
2021-12-28 15:02:49 +0530

    OAuth client created

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -5,7 +5,31 @@ import {FcGoogle } from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4';
 import logo from '../assets/logowhite.png';
 
+// import { client } from '../client';
+
 const Login = () => {
+
+    const navigate = useNavigate();
+    const responseGoogle = (response) => {
+
+      // console.log(response); in console, it displays all info of account that is logged in, like access token, google id, profileObj, tokenId, tokenObj
+
+      // making use of above info  in localStorage. Why? Because we want to create a new Sanity document
+      localStorage.setItem('user', JSON.stringify(response.profileObj));
+      // destructuring some of the documents
+      const { name, googleId, imageUrl } = response.profileObj;
+      // new Sanity document
+      const doc = {
+        _id: googleId,
+        _type: 'user',
+        userName: name,
+        image: imageUrl,
+      };
+    //   client.createIfNotExists(doc).then(() => {
+    //     navigate('/', { replace: true });
+    //   });
+    };
+
     return (
         <div className="flex justify-start items-center flex-col h-screen">
         <div className=" relative w-full h-full">
@@ -18,6 +42,33 @@ const Login = () => {
             autoPlay
             className="w-full h-full object-cover"
           />
+
+<div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" />
+          </div>
+
+          <div className="shadow-2xl">
+            <GoogleLogin
+              clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in with google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            />
+          </div>
+          </div>
+
+
           </div>
           </div>
     )
