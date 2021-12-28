ccf0da129b985a5b3ae8724e5470bf0b85160a1e
“prakshh” <prakashdas18@gmail.com>
2021-12-28 13:05:34 +0530

    implementing Router importing Routes

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello World.
-        </h1>
+        <Routes>
+            <Route path="login" element={<Login />} />
+            <Route path="/*" element={<Home />} />
+        </Routes>
     )
 }
 
