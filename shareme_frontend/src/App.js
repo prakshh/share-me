caf5f93cc963b979b86165ad816ae38548b37fbc
“prakshh” <prakashdas18@gmail.com>
2021-12-28 12:05:20 +0530

    using Tailwind’s utility classes to style

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div>
+        <h1 className="text-3xl font-bold underline">
             Hello World.
-        </div>
+        </h1>
     )
 }
 
